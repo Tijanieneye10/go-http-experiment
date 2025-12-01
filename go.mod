7a94defe2ed7ce8115438d3afc88af67de177cef
Tijani Eneye Usman <brainyworld10@gmail.com>
2025-12-01 15:01:16 +0100

    wip

---
+++ go.mod
@@ -0,0 +1,5 @@
+module experiments
+
+go 1.25.4
+
+require github.com/russross/blackfriday v1.6.0
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+	"github.com/russross/blackfriday"
+)
+
+
+func main() {
+	
+	http.HandleFunc("/markdown", GenerateMarkdown)
+	http.Handle("/", http.FileServer(http.Dir(".")))
+
+	// http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
+	http.ListenAndServe(":8080", nil)
+}
+
+
+func GenerateMarkdown(w http.ResponseWriter, r *http.Request) {
+	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))	
+	w.Write([]byte(r.URL.Query().Get("q")))
+	w.Write(markdown)
+}
