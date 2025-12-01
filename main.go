c3a3a1ddd51c0089005e3a184f25c5a027d049f3
Tijani Eneye Usman <brainyworld10@gmail.com>
2025-12-01 15:41:40 +0100

    wip

--- main.go
+++ main.go
@@ -1,23 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	_"github.com/russross/blackfriday"
+	"os"
+
+	"github.com/russross/blackfriday"
 )
 
 
 func main() {
+
+	PORT := os.Getenv("PORT")
+
+	if PORT == "" {
+		PORT = "8080"
+	}
 	
 	http.HandleFunc("/markdown", GenerateMarkdown)
 	http.Handle("/", http.FileServer(http.Dir(".")))
 
 	// http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(fmt.Sprintf(":%s", PORT), nil)
 }
 
 
 func GenerateMarkdown(w http.ResponseWriter, r *http.Request) {
-	// markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))	
-	w.Write([]byte(r.URL.Query().Get("q")))
-	// w.Write(markdown)
+	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))	
+	// w.Write([]byte(r.URL.Query().Get("q")))
+	w.Write(markdown)
 }
